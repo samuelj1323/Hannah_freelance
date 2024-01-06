8859040a402de2e7c42f29e14b50782bbca622be
samuelj1323 <samuelj1323@gmail.com>
2024-01-06 15:38:22 -0600

    Adding in routes for different pages using react router dom

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import { ThemeProvider, CssBaseline, Stack } from "@mui/material";
 import theme from "./theme/theme"; // Import the custom theme
 import Header from "./components/Header/Header";
 import Landing from "./pages/Landing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline /> {/* Ensures consistent baseline styles */}
-      <Stack
-        style={{
-          backgroundColor: theme.palette.background.default,
-          minHeight: "100vh",
-        }}
-      >
-        <Header />
-        <Landing />
-        {/* Your other components and content */}
-      </Stack>
+      <BrowserRouter basename="/">
+        <CssBaseline /> {/* Ensures consistent baseline styles */}
+        <Stack
+          style={{
+            backgroundColor: theme.palette.background.default,
+            minHeight: "100vh",
+          }}
+        >
+          <Header />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/hello-world" element={<>Hello world</>} />
+          </Routes>
+          {/* Your other components and content */}
+        </Stack>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
