94ad486ceda9c012fcde25689d8c8774d3c66e2d
samuelj1323 <samuelj1323@gmail.com>
2024-02-07 07:40:00 -0600

    App is now integrated with firebase!

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import React from "react";
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme/theme"; // Import the custom theme
 import AppRouter from "./AppRouter";
-
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/firestore";
 function App() {
+  const getEntries = async () => {
+    const samplesCol = collection(db, "writing-samples");
+    const samplesSnapshot = await getDocs(samplesCol);
+    const samples = samplesSnapshot.docs.map((doc) => doc.data());
+    return samples;
+  };
+
+  React.useEffect(() => {
+    console.log(getEntries());
+  }, []);
   return (
     <ThemeProvider theme={theme}>
       <AppRouter />
